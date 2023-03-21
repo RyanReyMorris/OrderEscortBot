62e76a86f374a0adc9490f9516198dc0ae360e12
Egor Yakimov <ryanreymorris@gmail.com>
2023-03-21 12:24:42 +0500

    Initial commit

---
+++ src/main/java/com/github/ryanreymorris/orderescortbot/OrderEscortBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.ryanreymorris.orderescortbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderEscortBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderEscortBotApplication.class, args);
+	}
+
+}
